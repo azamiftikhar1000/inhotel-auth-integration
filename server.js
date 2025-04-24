e09fa8b2ea05a33386c6e6248df77e4d4e8f4197
Aadil-Hasun <aadilhasun@gmail.com>
2025-04-24 05:43:01 +0530

    feat: proxy both /public/v1 and /v1/public in production

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 // server.js
 const express = require("express");
 const path = require("path");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(
+  ["/public/v1", "/v1/public"],
+  createProxyMiddleware({
+    target: "https://platform-backend.inhotel.io",
+    changeOrigin: true,
+  })
+);
 // serve your webpack build
 app.use(express.static(path.join(__dirname, "dist")));
 
